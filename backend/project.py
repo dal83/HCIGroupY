38141303319b650ba452cf95dadef5d9e022b56c
Ron George Pile <71215377+pileroniel@users.noreply.github.com>
2024-04-22 00:30:24 -0400

    finished page2 js and html, mocked OH data from backend

--- backend/project.py
+++ backend/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for,jsonify
+from flask import Flask, render_template, url_for,jsonify,request
 from flask_cors import CORS 
 
 app = Flask(__name__)
@@ -6,7 +6,12 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data= [
+        {'oh_id':'cs223','queue':'10','constant':'2.4'},
+        {'oh_id':'cs323','queue':'5','constant':'3.4'}
+      ]
+    return jsonify(data)
+    # return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
@@ -21,9 +26,18 @@ def page1():
       ]
     return jsonify(data)
 
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
+@app.route('/page2',methods=['GET'])
+def get_OH_data():
+    course = request.args.get('course')
+
+    if course:
+        print('received query for', course)
+        # Return a JSON response with the received query parameter value
+        return jsonify({'oh_id':course,'queue':20, 'constant':3})
+    else:
+        # Return an error response if the query parameter is missing
+        return jsonify({'error': 'Missing or invalid query parameter: course'}), 400
+
 
 @app.route('/page3')
 def page3():
