d4005bb83daa7d42305231b59938f7210af9de4c
Debbie Lilly <126291855+dal83@users.noreply.github.com>
2024-04-24 18:19:52 -0400

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,24 +1,12 @@
+let countdown = 10;
 $(document).ready(function () {
   const searchParams = new URLSearchParams(window.location.search);
-
   let chosen_course = searchParams.get("course");
 
-
-
-  // axios
-  //   .get(`http://localhost:5001/page2?course=${chosen_course}`)
-  //   .then((response) => {
-
-  //   })
-  //   .catch((error) => {
-  //     console.error(`Error from fetch`, error);
-  //   });
   let response = JSON.parse(sessionStorage.getItem("data"));
-
   console.log(response);
 
   let courses = response;
-  //   data in the form of {'oh_id':course_name,'queue':0, 'constant':0}
   console.log(`Response data`, courses);
 
   for (const data of courses) {
@@ -32,7 +20,6 @@ $(document).ready(function () {
 
       let students_el = $("#students");
       let queue = data["respondents"];
-
       students_el.text(`${Number(queue)}`);
 
       let waiting_el = $("#waiting");
@@ -41,6 +28,9 @@ $(document).ready(function () {
       waiting_el.text(`${waiting} minutes.`);
     }
   }
+
+  // Redirect to index.html after 5 seconds
+  setTimeout(() => {
+    window.location.href = "index.html";
+  }, 10000);
 });
-
-
